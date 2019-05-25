889033aeaa1dd8dbd2a9421e981214a6f372faf2
Patrique Ouimet <patrique.ouimet@gmail.com>
2019-05-25 13:10:39 -0400

    Trigger travis

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -5,9 +5,9 @@ namespace Patoui\ChuckNorrisJokes;
 class JokeFactory
 {
     protected $jokes = [
-        'Some people wear Superman pajamas. Superman wears Chuck Norris pajamas.',
-        'Chuck Norris\' belly button is actually a power outlet.',
-        'Chuck Norris is the reason why Waldo is hiding.',
+        'Some people wear Superman pajamas. Superman wears Chuck Norris pajamas',
+        'Chuck Norris\' belly button is actually a power outlet',
+        'Chuck Norris is the reason why Waldo is hiding',
     ];
 
     public function __construct(array $jokes = null)
