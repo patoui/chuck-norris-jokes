8803347036c90b8d6612566dd6dbc18cc445fa22
Patrique Ouimet <patrique.ouimet@gmail.com>
2019-05-25 12:51:21 -0400

    Initial commit

---
+++ src/JokeFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Patoui\ChuckNorrisJokes;
+
+class JokeFactory
+{
+    protected $jokes = [
+        'Some people wear Superman pajamas. Superman wears Chuck Norris pajamas.',
+        'Chuck Norris\' belly button is actually a power outlet.',
+        'Chuck Norris is the reason why Waldo is hiding.',
+    ];
+
+    public function __construct(array $jokes = null)
+    {
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
+    }
+
+    public function getRandomJoke()
+    {
+        return $this->jokes[array_rand($this->jokes)];
+    }
+}
---
+++ tests/JokeFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Patoui\ChuckNorrisJokes\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Patoui\ChuckNorrisJokes\JokeFactory;
+
+class JokeFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_joke()
+    {
+        $jokes = new JokeFactory([
+            'This is a joke',
+        ]);
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertSame('This is a joke', $joke);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_joke()
+    {
+        $chuckNorrisJokes = [
+            'Some people wear Superman pajamas. Superman wears Chuck Norris pajamas.',
+            'Chuck Norris\' belly button is actually a power outlet.',
+            'Chuck Norris is the reason why Waldo is hiding.',
+        ];
+
+        $jokes = new JokeFactory();
+        $joke = $jokes->getRandomJoke();
+
+        $this->assertContains($joke, $chuckNorrisJokes);
+    }
+}
