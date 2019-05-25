7c3cb362b672f8c20e447a656409e701ab1ac0b3
Patrique Ouimet <patrique.ouimet@gmail.com>
2019-05-25 21:20:01 +0000

    Apply fixes from StyleCI

--- tests/JokeFactoryTest.php
+++ tests/JokeFactoryTest.php
@@ -3,10 +3,10 @@
 namespace Patoui\ChuckNorrisJokes\Tests;
 
 use GuzzleHttp\Client;
-use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use GuzzleHttp\Handler\MockHandler;
 use Patoui\ChuckNorrisJokes\JokeFactory;
 
 class JokeFactoryTest extends TestCase
